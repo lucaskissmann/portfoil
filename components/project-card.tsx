fbd003c5919d870dd15a464d30696a0c5f5a41ae
Lucas Kissmann <lucaskissmann@gmail.com>
2024-07-18 15:23:42 -0300

    fix: adjustments on card style

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -24,7 +24,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   }
 
   return ( 
-    <div className="bg-white group rounded-xl border p-3 space-y-4 w-full">
+    <div className="bg-white group rounded-xl border p-3 flex flex-col w-full">
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image 
           src={image}
@@ -33,17 +33,17 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           className="aspect-square object-cover rounded-md"
         />
       </div>
-			<div>
+			<div className="flex-grow">
 				<h1 className="text-2xl font-ttsupermolot-bold">{title}</h1>
 				<p className="text-sm text-muted-foreground">
 					{technologies}
 				</p>
-			</div>
-      <div>
-        {description}
+				<div className="mt-4">
+					{description}
+				</div>
       </div>
 			<Button 
-				className="w-full text-lg font-ttsupermolot-normal bg-[#201F53] hover:bg-[#33326b]"
+				className="w-full mt-4 text-lg font-ttsupermolot-normal bg-[#201F53] hover:bg-[#33326b]"
 				onClick={handleClick}
 			>
 				Acessar
